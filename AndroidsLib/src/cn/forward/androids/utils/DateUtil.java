7ac69e53c8afbf7898a903f6980f138bca77e4be
huangziwei <154330138@qq.com>
2016-04-19 17:23:39 +0800

    修复了DateUtil判断日期错误的问题

--- AndroidsLib/src/cn/forward/androids/utils/DateUtil.java
+++ AndroidsLib/src/cn/forward/androids/utils/DateUtil.java
@@ -3,6 +3,7 @@ package cn.forward.androids.utils;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * 日期工具类
@@ -113,24 +114,24 @@ public class DateUtil {
 	}
 
     /**
-     * 判断日期是否为今天
-     *  @param time
-     * @return
+     * 根据系统默认时区，获取当前时间与time的天数差
+     *
+     * @param time
+     * @return　等于０表示今天，大于０表示今天之前
      */
-    public static boolean isToday(long time) {
-        // 24*60*60*1000
-        return (System.currentTimeMillis() - time) / 86400000 == 0;
+    public static long getDaySpan(long time) {
+        // 系统默认时区，ms
+        int tiemzone = TimeZone.getDefault().getRawOffset();
+        // １天＝24*60*60*1000ms
+        return (System.currentTimeMillis() + tiemzone) / 86400000
+                - (time + tiemzone) / 86400000;
     }
 
-    /**
-     * /**
-     * 判断日期是否为昨天
-     *  @param time
-     * @return
-     */
+    public static boolean isToday(long time) {
+        return getDaySpan(time) == 0;
+    }
+
     public static boolean isYestoday(long time) {
-        long span = (System.currentTimeMillis() - time) / 86400000;
-        System.out.println(span);
-        return span > 0 && span <= 1;
+        return getDaySpan(time) == 1;
     }
 }
